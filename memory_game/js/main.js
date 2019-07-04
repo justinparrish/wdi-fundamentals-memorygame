0d9834231c42cebb62399a26da721003385f2083
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-07-04 14:08:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-console.log("Up and running!");
-
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -18,8 +37,10 @@ checkForMatch();
 } else {
 	alert("Sorry, try again.")
 }
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].suit);
+console.log("User flipped " + cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 };
 flipCard(0);
 flipCard(2);
